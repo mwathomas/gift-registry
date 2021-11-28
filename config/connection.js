1e399d0a60bea81a732e37addd113d8bf9ac9623
M Abrams <madeline.a.thomas@gmail.com>
2021-11-28 15:53:23 -0500

    fixed connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,15 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: "localhost",
-    dialect: "mysql",
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+    }
+  );
 }
+
 module.exports = sequelize;
