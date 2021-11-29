3d18b385f14974615a9e75a0915cecad89d8a1ce
M Abrams <madeline.a.thomas@gmail.com>
2021-11-29 15:05:41 -0500

    added seed script

--- seeds/pickData.js
+++ seeds/pickData.js
@@ -1,26 +1,31 @@
-[
+const { Pick } = require("../models");
+
+const pickdata = [
   {
-    "title": "hot pink pillowcases",
-    "description": "i love these! especially that they are 100% cotton",
-    "url": "https://secure.img1-fg.wfcdn.com/im/67849114/resize-h445%5Ecompr-r85/1303/130319638/Adelay+100%2525+Cotton+Pillowcase.jpg",
-    "user_email": "madeline@gmail.com"
+    title: "hot pink pillowcases",
+    description: "i love these! especially that they are 100% cotton",
+    url: "https://secure.img1-fg.wfcdn.com/im/67849114/resize-h445%5Ecompr-r85/1303/130319638/Adelay+100%2525+Cotton+Pillowcase.jpg",
+    user_email: "madeline@gmail.com",
   },
   {
-    "title": "YSL crossbody bag",
-    "description": "for a splurge",
-    "url": "https://image.s5a.com/is/image/saks/0400012811702?wid=984&hei=1312&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
-    "user_email": "madeline@gmail.com"
+    title: "YSL crossbody bag",
+    description: "for a splurge",
+    url: "https://image.s5a.com/is/image/saks/0400012811702?wid=984&hei=1312&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
+    user_email: "madeline@gmail.com",
   },
   {
-    "title": "Fresh Flower Delivery",
-    "description": "Bouqs delivers a new arrangment monthly",
-    "url": "https://bouqs.com/cdn-cgi/image/format=webp/product_images/rainbow-ranunculus/Deluxe/60c16aeee083e00085da466c/detail.jpg?c=1623288558",
-    "user_email": "joe@gmail.com"
+    title: "Fresh Flower Delivery",
+    description: "Bouqs delivers a new arrangment monthly",
+    url: "https://bouqs.com/cdn-cgi/image/format=webp/product_images/rainbow-ranunculus/Deluxe/60c16aeee083e00085da466c/detail.jpg?c=1623288558",
+    user_email: "joe@gmail.com",
   },
   {
-    "title": "YSL crossbody bag",
-    "description": "for a splurge",
-    "url": "https://image.s5a.com/is/image/saks/0400012811702?wid=984&hei=1312&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
-    "user_email": "joe@gmail.com"
-  }
-]
+    title: "YSL crossbody bag",
+    description: "for a splurge",
+    url: "https://image.s5a.com/is/image/saks/0400012811702?wid=984&hei=1312&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0",
+    user_email: "joe@gmail.com",
+  },
+];
+const seedPick = () => Pick.bulkCreate(pickdata);
+
+module.exports = seedPick;
--- seeds/userData.js
+++ seeds/userData.js
@@ -1,22 +1,28 @@
-[
+const { User } = require("../models");
+
+const userdata = [
   {
-    "email": "madeline@gmail.com",
-    "password": "password12345"
+    email: "madeline@gmail.com",
+    password: "password12345",
   },
   {
-    "email": "joe@gmail.com",
-    "password": "hello"
+    email: "joe@gmail.com",
+    password: "hello",
   },
   {
-    "email": "amiko2k20@aol.com",
-    "password": "password12345"
+    email: "amiko2k20@aol.com",
+    password: "password12345",
   },
   {
-    "email": "jordan99@msn.com",
-    "password": "password12345"
+    email: "jordan99@msn.com",
+    password: "password12345",
   },
   {
-    "email": "the_blake@yahoo.com",
-    "password": "password12345"
-  }
-]
+    email: "the_blake@yahoo.com",
+    password: "password12345",
+  },
+];
+
+const seedUser = () => User.bulkCreate(userdata);
+
+module.exports = seedUser;
